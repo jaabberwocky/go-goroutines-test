8186cec3f720b528ba6198561c2a7a72bdd916a5
tobias <tobiasleongzhunmun@gmail.com>
2019-05-07 22:39:14 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Messing around with go routines
+
+Install by `go build`.
--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func f(from string) {
+
 	for i := 0; i < 5; i++ {
+		r := rand.Intn(10)
+		time.Sleep(time.Duration(r) * time.Millisecond)
 		fmt.Println(from, ":", i)
 	}
 }
 
+func printstuff(stuff string) {
+	r := rand.Intn(10)
+	time.Sleep(time.Duration(r) * time.Millisecond)
+	fmt.Println(stuff)
+}
+
 func main() {
+	f("blocking")
+
 	go f("first")
 	go f("second")
-	
+	go f("third")
+	go printstuff("stuff")
+
 	fmt.Scanln()
 	fmt.Println("done")
 }
